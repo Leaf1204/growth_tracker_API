1aaa5b14b48b48893c44d289fcf9b806e4adcd11
Leanne A Frisinger <67919464+Leaf1204@users.noreply.github.com>
2020-12-08 22:53:01 -0700

    scaffold child, gender and weight

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :children
+  resources :weights
+  resources :genders
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resource :users, only: [:create]
   post "/login", to: "users#login"
